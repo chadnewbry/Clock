732d3421c2ed8e1d18287f11143f2027e8d3f3d9
Chad Newbry <chad.newbry@gmail.com>
2022-11-14 11:05:23 -0500

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Chad Newbry on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clock/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Clock
+//
+//  Created by Chad Newbry on 11/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
